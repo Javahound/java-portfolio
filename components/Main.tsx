b70762eb46596cab58a668bbbe140ef5e3f503c6
Javahound <contact@javahound.eu>
2022-08-30 22:11:35 +0200

    Stuff

--- components/Main.tsx
+++ components/Main.tsx
@@ -21,8 +21,8 @@ const Main = () => {
                         <h1 className="mt-2">Hi, I&apos;m <span className="font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-[#576ad2] to-[#b075e7] gradient-move">Javahound</span><br />
                                              A Full Stack Web Dev</h1>
                         <div className="block w-full md:flex items-center">
-                            <div className="mt-8 mb-4 px-4 xs:px-8 max-w-[960px] md:w-[50%] lg:w-[60%]">
-                            <Image src="/avatarPicFullB.png" alt="A picture of my VRC Avatar" width='960' height='540' className="rounded-3xl" />
+                            <div className="mt-8 mb-4 px-4 xs:px-8 max-w-[960px] md:w-[75%] md:mx-auto lg:w-[60%]">
+                                <Image src="/avatarPicFullB.png" alt="A picture of my VRC Avatar" width='960' height='540' className="rounded-3xl" />
                             </div>
                             
                         </div>
@@ -30,7 +30,7 @@ const Main = () => {
                             <p className="mt-4 max-w-[75%] m-auto">I&apos;m a Web Dev, Software Engineer and (VR) Content Creator, at the moment focusing on building web experiences as well as making VR-Videos on Tiktok.<br />  </p>
                         </div>
                         <br />
-                        <TwitchStatus />
+                        {/* <TwitchStatus /> */}
                         <div className='pt-20'>
                             <p className='font-bold tracking-wider text-center text-transparent text-4xl bg-clip-text bg-gradient-to-r from-[#576ad2] to-[#b075e7]'>Let&apos;s Connect</p>
                             <p className="mt-2">Via my socials down below. Or maybe just take a look regardless. <br /> It&apos;d be much apprechiated. 😜</p>
@@ -59,7 +59,7 @@ const Main = () => {
                             <p id="about" className='font-bold tracking-wider text-center text-transparent text-4xl mt-20 bg-clip-text bg-gradient-to-r from-[#576ad2] to-[#b075e7]'>Who I am</p>
                             <div className="md:px-4 max-w-[960px] sm:w-full md:w-[50%] lg:w-[40%] mx-auto">
                                 <p className="text-center lg:px-2 mt-4">Me? I&apos;m a gay doggo on the interwebs :3 <br />
-                                I guess you could call me Part time Femboy<br />
+                                I guess I'm a Part time Femboy<br />
                                 Demi / Gay | {age} | <span className="font-bold">They</span> / <span className="font-bold">Them</span> | Taken <br />
                                 Java, Web, C#, TypeScript Dev</p>
                             </div>
